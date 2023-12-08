aa8e5fb44f9a30cd1143bc56f66ce95dc8de4f5b
Lord-Haji <lordhajisinbox@gmail.com>
2023-12-08 14:27:00 +0530

    Add new models

--- api/models.py
+++ api/models.py
@@ -12,8 +12,23 @@ class Scorecard(models.Model):
 
 class AudioFile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    file = models.FileField(upload_to='audio_files/')
-    uploaded_at = models.DateTimeField(auto_now_add=True)
+    file_name = models.CharField(max_length=255, default='unnamed_file')
+    drive_file_id = models.CharField(max_length=255, default='NOT_SET')  # Google Drive file ID
+    upload_date = models.DateTimeField(auto_now_add=True)
+    # Additional fields as needed
 
     def __str__(self):
-        return f"Audio file for {self.user.username} uploaded at {self.uploaded_at}"
+        return self.file_name
+
+
+
+class EvaluationResult(models.Model):
+    audio_file = models.ForeignKey(AudioFile, on_delete=models.CASCADE)
+    scorecard = models.ForeignKey(Scorecard, on_delete=models.SET_NULL, null=True)
+    result_data = models.JSONField()
+    evaluated_on = models.DateTimeField(auto_now_add=True)
+    # Additional fields as needed
+
+    def __str__(self):
+        return f"Result for {self.audio_file}"
+
--- api/urls.py
+++ api/urls.py
@@ -16,7 +16,6 @@ urlpatterns = [
     path('', include(router.urls)),
 
     path('register/', register, name='register'),
-    
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/blacklist/', TokenBlacklistView.as_view(), name='token_blacklist'),
