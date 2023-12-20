ab0dabf8ad5f62c861fcd871fdfba0f9c524030f
Lord-Haji <lordhajisinbox@gmail.com>
2023-12-20 14:45:30 +0530

    Add Support for Gemini Pro and use it by default

--- api/tasks.py
+++ api/tasks.py
@@ -2,7 +2,9 @@ from .models import Evaluation, AudioFile, Scorecard
 from django.utils import timezone
 import json
 
+import google.generativeai as genai
 from openai import OpenAI
+
 client = OpenAI()
 
 def perform_evaluation(user, audio_file_ids, scorecard_id, evaluation):
@@ -87,15 +89,53 @@ class ScorecardEvaluator:
             f"being the option you chose and reason being the reason you chose that option\n"
         )
         user_prompt = f"Here is the transcript: \n{self.transcript}"
+        prompt = f"{sys_prompt}\n{user_prompt}"
         
-        response = client.chat.completions.create(
-            model="gpt-4-1106-preview",
-            response_format={ "type": "json_object" },
-            messages=[
-                {"role": "system", "content": sys_prompt},
-                {"role": "user", "content": user_prompt}
-            ],
+        messages = [
+            {'role':'user',
+             'parts': [prompt]}
+        ]
+
+        generation_config = genai.GenerationConfig(
             temperature=0
         )
-        response_dict = json.loads(response.choices[0].message.content)
+
+        model = genai.GenerativeModel(model_name="gemini-pro",
+                                      generation_config=generation_config)
+
+        response = model.generate_content(messages)
+        print("Hi from Gemini-Pro")
+        
+        response_dict = json.loads(response.text.strip('`').replace('json\n', ''))
         return response_dict
+    
+    # def evaluate_gpt(self):
+    #     self.transcribe()
+    #     self.construct_prompt()
+        
+    #     schema_string = [{"question":"string","options":["string"],"llm_response":"string","reason":"string"}]
+         
+    #     sys_prompt = (
+    #         f"You are a Quality Assurance Analyst who is tasked evaluate the transcript "
+    #         f"based on the following questions and choose a given option with proper reasoning. \n"
+    #         f"{self.questions_and_options}\n"
+    #         f"Your Output should be in JSON with the keys being "
+    #         f"question(''), options([]), llm_response('') and reason('')\n"
+    #         f"In the following JSON Schema: for every question:"
+    #         f"{{'questions': {schema_string}}}"
+    #         f"With the question and options being the original ones provided and llm_response "
+    #         f"being the option you chose and reason being the reason you chose that option\n"
+    #     )
+    #     user_prompt = f"Here is the transcript: \n{self.transcript}"
+        
+    #     response = client.chat.completions.create(
+    #         model="gpt-4-1106-preview",
+    #         response_format={ "type": "json_object" },
+    #         messages=[
+    #             {"role": "system", "content": sys_prompt},
+    #             {"role": "user", "content": user_prompt}
+    #         ],
+    #         temperature=0
+    #     )
+    #     response_dict = json.loads(response.choices[0].message.content)
+    #     return response_dict
