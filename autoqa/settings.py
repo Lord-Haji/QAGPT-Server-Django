94f60294798f418c9659b01db66842709ce9fb1f
Lord-Haji <lordhajisinbox@gmail.com>
2023-12-28 12:07:18 +0530

    Add support for postgres, replacing sqlite3

--- autoqa/settings.py
+++ autoqa/settings.py
@@ -109,8 +109,12 @@ WSGI_APPLICATION = "autoqa.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "mydatabase",
+        "USER": "postgres",
+        "PASSWORD": "postgres",  # Replace with the password you set for the postgres user
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
