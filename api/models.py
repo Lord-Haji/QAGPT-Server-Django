d83355eca15bc02b3eef9d017d3fc6ad123a511a
Lord-Haji <lordhajisinbox@gmail.com>
2024-02-15 17:20:07 +0530

    Remove redundant legacy model field

--- api/models.py
+++ api/models.py
@@ -85,10 +85,6 @@ class Transcript(models.Model):
     audio_file = models.OneToOneField(
         AudioFile, on_delete=models.CASCADE, related_name="transcript"
     )
-    # formatted_text = models.TextField(
-    #     blank=True
-    # )  # Text with Diarization and Timestamps
-
     def __str__(self):
         return f"Transcript for {self.audio_file.file_name}"
 
