0b781b072ba831374a41aaf6308de9bf32bd3000
Lord-Haji <lordhajisinbox@gmail.com>
2024-02-26 19:31:36 +0530

    Disable direct login via 'token/' route

--- api/urls.py
+++ api/urls.py
@@ -33,7 +33,7 @@ router.register(r"transcripts", TranscriptViewSet)
 urlpatterns = [
     path("", include(router.urls)),
     path("register/", register, name="register"),
-    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    # path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("token/blacklist/", TokenBlacklistView.as_view(), name="token_blacklist"),
     path("auth/", include("trench.urls")),
